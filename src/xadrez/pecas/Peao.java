81eec6055f6d2506b4e633db16fed93057dd17b3
Deivide <dejs_silva@hotmail.com>
2023-06-09 15:15:39 -0300

    Peão correção

--- src/xadrez/pecas/Peao.java
+++ src/xadrez/pecas/Peao.java
@@ -20,12 +20,12 @@ public class Peao extends PecaXadrez{
 		
 		if(getCor() == Cor.BRANCO) {
 			p.setValues(posicao.getLinha() - 1, posicao.getColuna());
-			if(getTabuleiro().posicaoExistente(p) && getTabuleiro().haUmaPeca(p)) {
+			if(getTabuleiro().posicaoExistente(p) && !getTabuleiro().haUmaPeca(p)) {
 				mat[p.getLinha()][p.getColuna()] = true;
 			}
 				p.setValues(posicao.getLinha() - 2, posicao.getColuna());
 				Posicao p2 = new Posicao(posicao.getLinha() -1, posicao.getColuna());
-				if(getTabuleiro().posicaoExistente(p) && getTabuleiro().haUmaPeca(p) && getTabuleiro().posicaoExistente(p2) && getTabuleiro().haUmaPeca(p2) && getContagemMovimentos() == 0) {
+				if(getTabuleiro().posicaoExistente(p) && !getTabuleiro().haUmaPeca(p) && getTabuleiro().posicaoExistente(p2) && !getTabuleiro().haUmaPeca(p2) && getContagemMovimentos() == 0) {
 					mat[p.getLinha()][p.getColuna()] = true;
 				}
 				p.setValues(posicao.getLinha() - 1, posicao.getColuna() - 1);
@@ -39,12 +39,12 @@ public class Peao extends PecaXadrez{
 		}
 		else {
 			p.setValues(posicao.getLinha() + 1, posicao.getColuna());
-			if(getTabuleiro().posicaoExistente(p) && getTabuleiro().haUmaPeca(p)) {
+			if(getTabuleiro().posicaoExistente(p) && !getTabuleiro().haUmaPeca(p)) {
 				mat[p.getLinha()][p.getColuna()] = true;
 			}
 				p.setValues(posicao.getLinha() + 2, posicao.getColuna());
 				Posicao p2 = new Posicao(posicao.getLinha() +1, posicao.getColuna());
-				if(getTabuleiro().posicaoExistente(p) && getTabuleiro().haUmaPeca(p) && getTabuleiro().posicaoExistente(p2) && getTabuleiro().haUmaPeca(p2) && getContagemMovimentos() == 0) {
+				if(getTabuleiro().posicaoExistente(p) && !getTabuleiro().haUmaPeca(p) && getTabuleiro().posicaoExistente(p2) && !getTabuleiro().haUmaPeca(p2) && getContagemMovimentos() == 0) {
 					mat[p.getLinha()][p.getColuna()] = true;
 				}
 				p.setValues(posicao.getLinha() + 1, posicao.getColuna() - 1);
