77b7d935330d63b3bba55b94bdbf1aae5e747e86
Deivide <dejs_silva@hotmail.com>
2023-06-09 13:01:59 -0300

    Lógica de xeque e correção capturar peças

--- src/xadrez/PecaXadrez.java
+++ src/xadrez/PecaXadrez.java
@@ -17,7 +17,10 @@ public abstract class PecaXadrez extends Peca {
 		return cor;
 	}
 	
-
+	public XadrezPosicao getXadrezPosicao() {
+		return  XadrezPosicao.daPosicao(posicao);
+	}
+	
 	protected boolean seExistePecaAdversaria(Posicao posicao) {
 		PecaXadrez p = (PecaXadrez)getTabuleiro().peca(posicao);
 		return p != null && p.getCor() != cor;
